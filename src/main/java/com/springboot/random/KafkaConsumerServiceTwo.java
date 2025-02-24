ba34fc0009da97c6cdaf4d657aa4fcaa41e2a149
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-02-24 20:43:31 +0530

    Kafka consume by two different srervice or group id

--- src/main/java/com/springboot/random/KafkaConsumerServiceTwo.java
+++ src/main/java/com/springboot/random/KafkaConsumerServiceTwo.java
@@ -4,11 +4,10 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaConsumerService {
+public class KafkaConsumerServiceTwo {
 
-    @KafkaListener(topics = "test-topic", groupId = "my-group")
+    @KafkaListener(topics = "test-topic", groupId = "my-group2")
     public void listen(String message) {
-        System.out.println("Consumed message by Service 1: " + message);
+        System.out.println("Consumed message by Service 2: " + message);
     }
-
 }
